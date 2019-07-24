46eccf5f270e5e3dde1c4d797eecd628629e7320
rob <robert@lifting-sea.com>
2019-07-24 15:18:18 -0700

    add post_type reference

--- lib/wp_api_client/entities/post_type.rb
+++ lib/wp_api_client/entities/post_type.rb
@@ -7,9 +7,6 @@ module WpApiClient
         json.key?("hierarchical") and json.key?("name") and json.key?("slug")
       end
 
-      def initialize(json)
-        raise WpApiClient::ErrorResponse.new(json)
-      end
     end
   end
 end
